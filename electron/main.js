dd10e3abb1cd4a7fd27552cd5e8e39f480ea37c4
salernoelia <putopet@gmail.com>
2024-06-27 00:19:27 +0200

    electron adjustments

--- electron/main.js
+++ electron/main.js
@@ -1,23 +1,56 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const { exec } = require("child_process");
+const waitOn = require("wait-on");
+
+let win;
 
 function createWindow() {
-  // Create the browser window.
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 1400,
     height: 1050,
+    title: "Ollama",
     webPreferences: {
       nodeIntegration: true,
     },
   });
 
-  // Load the index.html of the app.
-  // Update the path below to where your Nuxt output is served
-  win.loadURL("http://localhost:3000"); // Adjust port if your app is served differently
+  win.loadURL("http://localhost:3000");
 
-  // Open the DevTools.
+  // Uncomment to open DevTools
   // win.webContents.openDevTools();
 }
-app.on("ready", createWindow);
+
+app.on("ready", () => {
+  // Determine the path to the server script
+  const serverPath = path.join(app.getAppPath(), ".output/server/index.mjs");
+  console.log("Server path:", serverPath); // Log the server path for debugging
+
+  // Start Nuxt server
+  const nuxtServer = exec(`node ${serverPath}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
+
+  nuxtServer.on("close", (code) => {
+    console.log(`Nuxt server process exited with code ${code}`);
+  });
+
+  // Wait for the Nuxt server to be ready and then create the window
+  waitOn({ resources: ["http://localhost:3000"], timeout: 30000 }, (err) => {
+    if (err) {
+      console.error("Nuxt server did not start in time");
+      nuxtServer.kill();
+      app.quit();
+    } else {
+      createWindow();
+    }
+  });
+});
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
